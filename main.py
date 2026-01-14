6c708de9c6d0b77d3f2f0f59c083a575c355e405
xjdsld <generalmailbox@ukr.net>
2026-01-14 19:20:39 +0200

    Create Player class for managing player state
    Refactor player placement into Player class with methods.

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-def place_player(field, user_n, user_m, skin):
-    field[user_n][user_m] = skin
+class Player:
+    def __init__(self, user_n=0, user_m=0, skin="*"):
+        self.user_n = row
+        self.user_m = col
+        self.skin = skin
+
+    def place(self, field):
+        field[self.row][self.col] = self.skin
