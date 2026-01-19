a094cd530ce16e5c9eaa4fb69fb8fc7d5a91b68a
xjdsld <generalmailbox@ukr.net>
2026-01-19 16:47:07 +0200

    Delete Player_Class.py

--- Player_Class.py
+++
@@ -1,8 +0,0 @@
-class Player:
-    def __init__(self, user_n=0, user_m=0, skin="*"):
-        self.user_n = row
-        self.user_m = col
-        self.skin = skin
-
-    def place(self, field):
-        field[self.row][self.col] = self.skin
